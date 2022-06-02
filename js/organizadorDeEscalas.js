33f62c32404fb6ce282639689917b967769acaf6
JadiellyL <desenvolvedorajadielly@gmail.com>
2022-06-01 21:08:01 -0300

    Background linear gradient

---
+++ js/organizadorDeEscalas.js
@@ -0,0 +1,21 @@
+:root{
+  --cor-fundo:   #f8f6f6;
+}
+
+body{
+  background-color: var(--cor-fundo);
+  text-align: center;
+}
+
+ul{
+  list-style-type: none;
+}
+
+section{
+  width: 600px;
+  background-color: #ff006681;
+  min-width: 400px;
+  padding: 10px;
+  margin: 0 auto;
+
+}
